a66bf95bcdf7ca9f1167bd1968aa33e315846eaa
George Cairns <george.cairns@gmail.com>
2012-03-18 20:57:18 +0000

    Added head, made buildlines better (but still slow)

--- Assets/BuildLines.js
+++ Assets/BuildLines.js
@@ -2,6 +2,7 @@
 
 var c1 : Color = Color.yellow;
 var c2 : Color = Color.red;
+var child : Transform;
 //var lengthOfLineRenderer : int = 20;
 
 // Comment 2
@@ -11,7 +12,12 @@ var c2 : Color = Color.red;
 function Start() {
 
 	MakeLineRenderer(transform , gameObject.AddComponent(LineRenderer) as LineRenderer , 0);
-
+	
+	while (1) {
+		//UpdateLineRenderer(transform , gameObject.GetComponent(LineRenderer) as LineRenderer , 0);
+		yield WaitForSeconds(0.5);
+	}
+	
 }
 
 
@@ -20,7 +26,7 @@ function MakeLineRenderer(currentNode:Transform , line : LineRenderer,nodeNumber
 	if (nodeNumber == 0) {
 		line.material = new Material (Shader.Find("Particles/Additive"));
 		line.SetColors(c1, c2);
-		line.SetWidth(0.2,0.2);
+		line.SetWidth(0.05,0.05);
 			
 	}
 	
@@ -37,7 +43,7 @@ function MakeLineRenderer(currentNode:Transform , line : LineRenderer,nodeNumber
 		case 1:
 			//continue the current line
 			nodeNumber++;
-			for (var child : Transform in currentNode) {
+			for (child  in currentNode as Transform ) {
     			MakeLineRenderer(child,line,nodeNumber);
     			Debug.Log("continuing line through " + currentNode.name);
 			}
@@ -45,10 +51,15 @@ function MakeLineRenderer(currentNode:Transform , line : LineRenderer,nodeNumber
 		
 		default:
 			//start new rendererers
-			for (var child : Transform in currentNode as Transform) {
-			
-				Debug.Log("Building new line from " + currentNode.name);
-    			MakeLineRenderer(child,child.gameObject.AddComponent(LineRenderer) as LineRenderer,0);
+			for (child in currentNode as Transform ) {
+				if (currentNode.name == 'Head'){
+					Debug.Log("Destroying "+ child.gameObject.name);
+					Destroy(child.gameObject);
+					
+				} else {
+					Debug.Log("Building new line from " + currentNode.name);
+    				MakeLineRenderer(child,child.gameObject.AddComponent(LineRenderer) as LineRenderer,0);
+    			}
 			}
 			
 		break;	
@@ -60,26 +71,25 @@ function MakeLineRenderer(currentNode:Transform , line : LineRenderer,nodeNumber
 function Update() {
 
 	UpdateLineRenderer(transform , gameObject.GetComponent(LineRenderer) as LineRenderer , 0);
-
 }
 
 
 function UpdateLineRenderer(currentNode:Transform , line : LineRenderer,nodeNumber : int)  {
 
-	
+	Debug.Log(line.gameObject.name);
 	line.SetPosition(nodeNumber,currentNode.position);
 	
 	switch (currentNode.childCount) 
 	{
 		case 0:
 			//the end of the node, finish the linerenderer
-			Debug.Log("ending line at " + currentNode.name);
+			//Debug.Log("ending line at " + currentNode.name);
 		break;
 		
 		case 1:
 			//continue the current line
 			nodeNumber++;
-			for (var child : Transform in currentNode) {
+			for (child in currentNode as Transform ) {
     			UpdateLineRenderer(child,line,nodeNumber);
     			//Debug.Log("continuing line through " + currentNode.name);
 			}
@@ -87,10 +97,12 @@ function UpdateLineRenderer(currentNode:Transform , line : LineRenderer,nodeNumb
 		
 		default:
 			//start new rendererers
-			for (var child : Transform in currentNode as Transform) {
 			
-				//Debug.Log("Building new line from " + currentNode.name);
-    			UpdateLineRenderer(child,child.gameObject.GetComponent(LineRenderer) as LineRenderer,0);
+			for (child in currentNode as Transform ) {
+
+					//Debug.Log("Building new line from " + currentNode.name);
+    				UpdateLineRenderer(child,child.gameObject.GetComponent(LineRenderer) as LineRenderer,0);
+    			
 			}
 			
 		break;	

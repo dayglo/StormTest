82e0bb70f7f2e8a30ec322b7928b739de518a781
George Cairns <george.cairns@gmail.com>
2012-03-19 01:28:43 +0000

    got rid of hinting and made the lines build correctly
    by making the tree traversal a bit smarter.

--- Assets/player/scripts/BuildLines.js
+++ Assets/player/scripts/BuildLines.js
@@ -2,22 +2,10 @@
 
 var c1 : Color = Color.yellow;
 var c2 : Color = Color.red;
-var lineContinuationHintKeys : Transform[];
-var lineContinuationHintValues : Transform[];
-var i : int; //iterator
-
 
 function Start() {
 	
-	Debug.Log(lineContinuationHintKeys.Length);
-	
-	
 	MakeLineRenderer(transform , gameObject.AddComponent(LineRenderer) as LineRenderer , 0);
-	
-	while (1) {
-		//UpdateLineRenderer(transform , gameObject.GetComponent(LineRenderer) as LineRenderer , 0);
-		yield WaitForSeconds(0.5);
-	}
 	
 }
 
@@ -28,6 +16,8 @@ function MakeLineRenderer(currentNode:Transform , line : LineRenderer,nodeNumber
 		line.material = new Material (Shader.Find("Mobile/VertexLit"));
 		line.SetColors(c1, c2);
 		line.SetWidth(0.05,0.05);
+		
+
 			
 	}
 	
@@ -53,34 +43,17 @@ function MakeLineRenderer(currentNode:Transform , line : LineRenderer,nodeNumber
 		break;
 		
 		default:
-
-			// work out if there is a hinted child for this node.
-			var hintedChild : Transform;			
-			for (i=0;i<lineContinuationHintKeys.Length;i++) {
-				if (lineContinuationHintKeys[i] == currentNode) {
-					//hint found, store hinted child for use in child enumerator loop
-					Debug.Log("hint found, continue line from " +currentNode.name + " to " +lineContinuationHintValues[i].name);
-					hintedChild = lineContinuationHintValues[i];		
-				}
-				
-			}
 			
 			//iterate children and work out what to do with each one.
 			for (var child : Transform in currentNode) {
 				
-				if (child == hintedChild) {
-				
-					nodeNumber++;
-					MakeLineRenderer(hintedChild,line,nodeNumber);;
-				
-				} else {
 					//delete all the head subnodes, they arent needed.
 					if (currentNode.name == 'Head'){
 						Debug.Log("Destroying "+ child.gameObject.name);
 						Destroy(child.gameObject);
 						
 					} else {
-						//a hint wasnt found, so make a new line
+						//make a new line
 						Debug.Log("Building new line from " + currentNode.name);
 						var newLine = child.gameObject.AddComponent(LineRenderer) as LineRenderer;
 						
@@ -89,11 +62,16 @@ function MakeLineRenderer(currentNode:Transform , line : LineRenderer,nodeNumber
 						
 						newLine.material = new Material (Shader.Find("Mobile/VertexLit"));
 						newLine.SetColors(c1, c2);
-						newLine.SetWidth(0.05,0.05);
+						newLine.SetWidth(0.03,0.03);
+						
+								if (currentNode.name.Contains("Hand")){
+									newLine.SetWidth(0.02,0.02);
+	
+								}
 						
 						MakeLineRenderer(child,newLine,1);
 	    			}
-				}
+				
 			}
 			
 		break;	
@@ -103,7 +81,7 @@ function MakeLineRenderer(currentNode:Transform , line : LineRenderer,nodeNumber
 
 function Update() {
 
-	//UpdateLineRenderer(transform , gameObject.GetComponent(LineRenderer) as LineRenderer , 0);
+	UpdateLineRenderer(transform , gameObject.GetComponent(LineRenderer) as LineRenderer , 0);
 }
 
 
@@ -127,7 +105,9 @@ function UpdateLineRenderer(currentNode:Transform , line : LineRenderer,nodeNumb
 		default:
 			//start new rendererers
 			for (var child : Transform in currentNode) {
-    				UpdateLineRenderer(child,child.gameObject.GetComponent(LineRenderer) as LineRenderer,0);
+				var newLine = child.gameObject.GetComponent(LineRenderer) as LineRenderer;
+    			newLine.SetPosition(0,currentNode.position);
+    			UpdateLineRenderer(child,newLine,1);
 			}
 		break;	
 	

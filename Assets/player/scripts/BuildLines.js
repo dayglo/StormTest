4f23cf2f045585ffcc6f8c4b5e1a1e5a6af8b587
George Cairns <george.cairns@gmail.com>
2012-03-19 01:00:04 +0000

    added line hinting step 1
    to stop the guy having empty space between lines.

--- Assets/player/scripts/BuildLines.js
+++ Assets/player/scripts/BuildLines.js
@@ -2,15 +2,16 @@
 
 var c1 : Color = Color.yellow;
 var c2 : Color = Color.red;
-var child : Transform;
-//var lengthOfLineRenderer : int = 20;
-
-// Comment 2
-// Comment 4
+var lineContinuationHintKeys : Transform[];
+var lineContinuationHintValues : Transform[];
+var i : int; //iterator
 
 
 function Start() {
-
+	
+	Debug.Log(lineContinuationHintKeys.Length);
+	
+	
 	MakeLineRenderer(transform , gameObject.AddComponent(LineRenderer) as LineRenderer , 0);
 	
 	while (1) {
@@ -33,6 +34,7 @@ function MakeLineRenderer(currentNode:Transform , line : LineRenderer,nodeNumber
 	line.SetVertexCount(nodeNumber+1);
 	line.SetPosition(nodeNumber,currentNode.position);
 	
+	Debug.Log("The current node ("+ currentNode.name +") has " + currentNode.childCount + " children");
 	switch (currentNode.childCount) 
 	{
 		case 0:
@@ -44,46 +46,65 @@ function MakeLineRenderer(currentNode:Transform , line : LineRenderer,nodeNumber
 			//continue the current line
 			nodeNumber++;
 			for (var child : Transform in currentNode) {
+				Debug.Log("continuing line through " + currentNode.name);
     			MakeLineRenderer(child,line,nodeNumber);
-    			Debug.Log("continuing line through " + currentNode.name);
+
 			}
 		break;
 		
 		default:
-			//start new rendererers
+
+			// work out if there is a hinted child for this node.
+			var hintedChild : Transform;			
+			for (i=0;i<lineContinuationHintKeys.Length;i++) {
+				if (lineContinuationHintKeys[i] == currentNode) {
+					//hint found, store hinted child for use in child enumerator loop
+					Debug.Log("hint found, continue line from " +currentNode.name + " to " +lineContinuationHintValues[i].name);
+					hintedChild = lineContinuationHintValues[i];		
+				}
+				
+			}
+			
+			//iterate children and work out what to do with each one.
 			for (var child : Transform in currentNode) {
-				if (currentNode.name == 'Head'){
-					Debug.Log("Destroying "+ child.gameObject.name);
-					Destroy(child.gameObject);
-					
+				
+				if (child == hintedChild) {
+				
+					nodeNumber++;
+					MakeLineRenderer(hintedChild,line,nodeNumber);;
+				
 				} else {
-					Debug.Log("Building new line from " + currentNode.name);
-    				MakeLineRenderer(child,child.gameObject.AddComponent(LineRenderer) as LineRenderer,0);
-    			}
+					//delete all the head subnodes, they arent needed.
+					if (currentNode.name == 'Head'){
+						Debug.Log("Destroying "+ child.gameObject.name);
+						Destroy(child.gameObject);
+						
+					} else {
+						//a hint wasnt found, so make a new line
+						Debug.Log("Building new line from " + currentNode.name);
+						MakeLineRenderer(child,child.gameObject.AddComponent(LineRenderer) as LineRenderer,0);
+	    			}
+				}
 			}
 			
 		break;	
-	
 	}
 
 }
 
 function Update() {
 
-	UpdateLineRenderer(transform , gameObject.GetComponent(LineRenderer) as LineRenderer , 0);
+	//UpdateLineRenderer(transform , gameObject.GetComponent(LineRenderer) as LineRenderer , 0);
 }
 
 
 function UpdateLineRenderer(currentNode:Transform , line : LineRenderer,nodeNumber : int)  {
 
-	//Debug.Log(line.gameObject.name);
 	line.SetPosition(nodeNumber,currentNode.position);
 	
 	switch (currentNode.childCount) 
 	{
 		case 0:
-			//the end of the node, finish the linerenderer
-			//Debug.Log("ending line at " + currentNode.name);
 		break;
 		
 		case 1:
@@ -91,20 +112,14 @@ function UpdateLineRenderer(currentNode:Transform , line : LineRenderer,nodeNumb
 			nodeNumber++;
 			for (var child : Transform in currentNode) {
     			UpdateLineRenderer(child,line,nodeNumber);
-    			//Debug.Log("continuing line through " + currentNode.name);
 			}
 		break;
 		
 		default:
 			//start new rendererers
-			
 			for (var child : Transform in currentNode) {
-
-					//Debug.Log("Building new line from " + currentNode.name);
     				UpdateLineRenderer(child,child.gameObject.GetComponent(LineRenderer) as LineRenderer,0);
-    			
 			}
-			
 		break;	
 	
 	}

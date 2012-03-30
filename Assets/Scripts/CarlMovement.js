226d515e1d62fba3aa53463a4943a1ecb0e08720
Simon Parkinson <simonparkinson@scpsoft.com>
2012-03-30 15:28:47 +0100

    New Carl
    New running
    Tap to get a normal Jump
    Double tap gives a extended jump

--- Assets/Scripts/CarlMovement.js
+++ Assets/Scripts/CarlMovement.js
@@ -1,10 +1,7 @@
 #pragma strict
 private var Carl : GameObject;
-var walkSpeed : float = 2.0;
 
-function Update(){
-	Carl.animation["Walk"].speed = walkSpeed;
-}
+
 
 function Awake() {
 Carl = GameObject.FindGameObjectWithTag("Player");
@@ -15,11 +12,19 @@ Carl = GameObject.FindGameObjectWithTag("Player");
 function Start(){
 
 //set the wrapmode of the different animations
-Carl.animation["Run"].wrapMode =WrapMode.Loop;
-Carl.animation["Walk"].wrapMode =WrapMode.Loop;
+Carl.animation["run"].wrapMode =WrapMode.Loop;
+Carl.animation["walk"].wrapMode =WrapMode.Loop;
+Carl.animation["walk"].speed = 1.7;
+//Carl.animation["run"].speed = 1.7;
+
+Carl.animation.Play("run");
 
 
-Carl.animation.Play("Walk");
+}
+
+function Update() {
+
+
 
 
 }
@@ -28,16 +33,30 @@ Carl.animation.Play("Walk");
 function Jump() {
 	
 
-	Carl.animation.CrossFade("Jump");
-	
-	
-	
-	Carl.animation.CrossFadeQueued("Walk",0.3,QueueMode.CompleteOthers);
-	
-	
+	Carl.animation.CrossFade("runandjump");
+	Carl.animation.CrossFadeQueued("run",0.3,QueueMode.CompleteOthers);
+	//Carl.animation["walk"].speed = 1.7;
+	//Carl.animation.Play("walk");
+
 	
 }
 
+function Run() {
+Carl.animation.CrossFade("runold");
+Carl.animation.Play("runold");
+//Carl.animation["run"].speed = 1.7;
+
+}
+
+
+function BigJump() {
+Carl.animation.CrossFade("bigjump");
+Carl.animation.CrossFadeQueued("run",0.3,QueueMode.CompleteOthers);
+//Carl.animation["run"].speed = 1.7;
+
+}
+
+/*
 function Punch() {
 	
 
@@ -46,13 +65,24 @@ function Punch() {
 	
 	
 }
+*/
 
 
+function Swat() {
+	
+	Carl.animation.Blend("swat",1.0f,0.1f);
+	//Carl.animation.CrossFade("swat");
+	//Carl.animation.CrossFadeQueued("run",0.3,QueueMode.CompleteOthers);
+	//Carl.animation["walk"].speed = 1.7;
+	//Carl.animation.Play("walk");
+
+	
+}
 function OnEnable()
 {
     // subscribe to the global tap event
     FingerGestures.OnTap += MyFingerGestures_OnTap;
-     // FingerGestures.OnDoubleTap += MyFingerGestures_OnDoubleTap;
+      FingerGestures.OnDoubleTap += MyFingerGestures_OnDoubleTap;
       FingerGestures.OnSwipe += MyFingerGestures_OnSwipe;
     
 }
@@ -61,7 +91,7 @@ function OnDisable()
 {
     // unsubscribe from the global tap event
     FingerGestures.OnTap -= MyFingerGestures_OnTap;
-      // FingerGestures.OnDoubleTap -= MyFingerGestures_OnDoubleTap;
+ FingerGestures.OnDoubleTap -= MyFingerGestures_OnDoubleTap;
         FingerGestures.OnSwipe -= MyFingerGestures_OnSwipe;
 }
  
@@ -74,9 +104,19 @@ function MyFingerGestures_OnTap( fingerPos : Vector2 )
     
 }
 
+
+function MyFingerGestures_OnDoubleTap( fingerPos : Vector2 )
+{
+  // Debug.Log( " Double TAP  at " + fingerPos );
+  // Punch();
+  BigJump();
+    
+}
+
 function MyFingerGestures_OnSwipe( fingerPos : Vector2 )
 {
   // Debug.Log( " Double TAP  at " + fingerPos );
-   Punch();
+  // Punch();
+  Swat();
     
 }

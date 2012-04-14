77995fb3c3b55021b485a2aee864f4cf8c842418
George Cairns <george.cairns@gmail.com>
2012-04-14 15:21:02 +0100

    butterfly prefab v3
    butterfly now points in the direction it is fluttering

--- Assets/environment/items/Butterfly/FlutterVelocity.js
+++ Assets/environment/items/Butterfly/FlutterVelocity.js
@@ -1,6 +1,6 @@
 var maxSpeed : float = 0.001;
 
-private var targetVelocity: Vector3;
+var targetVelocity: Vector3;
 private var currentVelocity: Vector3 = Vector3.zero;
 private var timeDelay : float = 1.0;
 private var myTransform : Transform;
@@ -11,6 +11,7 @@ var targetPosition :Vector3;
 private var vectorToTarget : Vector3; 
 
 private var correction : float;
+
 
 function Start () {
 	myTransform = transform;
@@ -48,10 +49,23 @@ function BuzzAround(){
 		  
 		Debug.DrawRay(myTransform.position,targetVelocity * 10,Color.blue,timeDelay);
 		
-
+		
+		//gameObject.GetComponents(iTweenEvent).Play();
+		
+		//iTween.RotateTo(gameObject,{"x":3,"time":4,"delay":1,"onupdate","myUpdateFunction","looptype","pingpong"});	
+		
+		
+		//iTweenEvent.GetEvent(gameObject, "rotateToVelocity").Play();
+		
+		
+		iTween.LookTo(gameObject,myTransform.position + targetVelocity,timeDelay);
+		
+		
 		targetVelocity.z = 0; 
 		
 
 		yield WaitForSeconds(timeDelay);
 	}
 }
+
+

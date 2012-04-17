69ab166688ed3213349bcfbfa99c45892ce7c1f2
George Cairns <george.cairns@gmail.com>
2012-04-17 16:40:56 +0100

    added river models/textures
    do not use yet

---
+++ Assets/player/scripts/ButterflyIndex.js
@@ -0,0 +1,20 @@
+#pragma strict
+
+var commonName : String[];
+var latinName : String[];
+var rarity : int[];
+var texture : Material;
+var size : int[];
+
+var butterflies = new Hashtable();   
+
+function Start () {
+
+butterflies.Add("name","");
+
+	
+}
+
+function Update () {
+
+}

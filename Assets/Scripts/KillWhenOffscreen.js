43703bcbcc0e803bd56a0c64cfe9d413b9e92db7
George Cairns <george.cairns@gmail.com>
2012-05-14 00:27:06 +0100

    fixed emitter timeouts

--- Assets/Scripts/KillWhenOffscreen.js
+++ Assets/Scripts/KillWhenOffscreen.js
@@ -1,5 +1,6 @@
 #pragma strict
 var parent2 : int = 0;
+var deathTimeOut : float = 1.1;
 
  var hasAppeared : int = 0;
 
@@ -11,10 +12,13 @@ function OnBecameInvisible() {
 	// Only kill if we have actually been made visible
 	if(hasAppeared) {
 		if (parent2 == 0) {
+			yield WaitForSeconds(deathTimeOut);
 			Destroy(transform.gameObject);
 		} else if (parent2 == 1) {
+			yield WaitForSeconds(deathTimeOut);
 			Destroy(transform.parent.gameObject);
 		} else if (parent2 == 2) {
+			yield WaitForSeconds(deathTimeOut);
 			Destroy(transform.parent.parent.gameObject);
 		}
 	
